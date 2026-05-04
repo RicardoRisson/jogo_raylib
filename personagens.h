7840057c5faaa439b6989558303902cc4b78d9f4
me50 <murilo.lanzarini@gmail.com>
2026-05-04 12:35:20 -0300

    sla

--- personagens.h
+++ personagens.h
@@ -6,5 +6,5 @@
 // --- Inicialização de Entidades ---
 // Calcula o spawn e dimensões iniciais do retângulo do jogador
 Rectangle criar_jogador(int largura_janela, int altura_janela, int largura_jogador, int altura_jogador);
-//teste
+
 #endif
