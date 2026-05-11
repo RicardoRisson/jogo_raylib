777e22fe40f61b0498eb96c25b6055f1abf42093
TorasPijas <murilo.lanzarini@gmail.com>
2026-05-11 16:12:31 -0300

    teste

--- movimentacao.h
+++ movimentacao.h
@@ -10,5 +10,5 @@ Rectangle atualizar_movimento(Rectangle jogador, float velocidade);
 // --- Lógica de Colisão ---
 // Ajusta altura do jogador e reseta aceleração ao tocar plataformas
 Rectangle verificar_chao(Rectangle jogador, Rectangle plataformas[], int quantidade_plataformas);
-
+//teste
 #endif
